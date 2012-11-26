9ce1592a0aceddd2019aa5a1c5963674bbf6679c
Doug <doug@doug-Inspiron-580.(none)>
2012-11-25 20:35:06 -0500

    Forgot To Add MProcessTiming class changes

--- moss/Time/MProcessTiming.java
+++ moss/Time/MProcessTiming.java
@@ -20,12 +20,19 @@ public class MProcessTiming{
 	//Somewhat arbitrarily choose maxPossibleCPURuns segments that would need to run on CPU
 	//for any given process. Also arbitrarily chose a max of maxPossibleProcesses processes
 	//that would be running at any given time.
-	static double[][][] MProcessSchedules= new double[maxPossibleProcesses][maxPossibleCPURuns][2]; 
+	static private double[][][] MProcessSchedules= new double[maxPossibleProcesses][maxPossibleCPURuns][2]; 
 	
 	//A string of process names that correspond to the first index of MProcessSchedules
 	//such that each of maxPossibleProcesses possible processes has maxPossibleCPURuns segments of time that it is running
 	//in the CPU.
-	static int[] MProcessScheduleMapping=new int[maxPossibleProcesses];
+	static private int[] MProcessScheduleMapping=new int[maxPossibleProcesses];
+	
+	/*
+	 * currentGlobalTime is initialized to 0 and increments when any process advances
+	 * by some number of time.
+	 * 
+	 */
+	static private double currentGlobalTime=0;
 	
 	/* 
 	 * processTime represents how much a given process has progressed to completion.
@@ -33,7 +40,7 @@ public class MProcessTiming{
 	 * process is running on the CPU such that it is possible that real time is still
 	 * incrementing while the process time is stagnant.
 	 */
-	static double[] processTime=new double[20]; 
+	static private double[] processTime=new double[20]; 
 	
 	//Initialize a single MProcess (for use when a Process has completed) or when initializing the 
 	//array that makes up the schedules for all the processes.
@@ -200,9 +207,15 @@ static public void addCPUTimeSlotOnProcess(int processID,int startTime,int endTi
 			processTimeLargerThanCurrentScheduleIndex=(processTime[processNum]>=MProcessSchedules[processNum][searchNextProcessTimeIndex][1])
 					&&MProcessSchedules[processNum][searchNextProcessTimeIndex][1]!=-1;
 		}
-		globalTimeAdvance=Math.min(MProcessSchedules[processNum][searchNextProcessTimeIndex][1]-processTime[processNum],advanceTimeAmount);
+		globalTimeAdvance=Math.max(Math.min(MProcessSchedules[processNum][searchNextProcessTimeIndex][1]-processTime[processNum],advanceTimeAmount),0);
+		
+		//advance the global time for all processes by the amount that 'processNum' ran on CPU.
+		currentGlobalTime=currentGlobalTime+globalTimeAdvance;
+		
 		processTime[processNum]=processTime[processNum]+globalTimeAdvance;
-
+		
+		//If other processes were doing I/O while 'processNum' was being advanced, 
+		//advance those processes to later processTime states.
 		advanceAllProcessesDoingIO(globalTimeAdvance,processNum);
 		
 		if(MProcessSchedules[processNum][searchNextProcessTimeIndex][0]==-1){
@@ -218,6 +231,18 @@ static public void addCPUTimeSlotOnProcess(int processID,int startTime,int endTi
 	}
 	
 	
+	//getGlobalTime returns the currentGlobalTime
+	static public double getGlobalTime(){
+		return currentGlobalTime;
+	}
+	
+	//reset currentGlobalTime to 0. Useful at the end of unit tests to keep global time unit
+	//test specific
+	static public void resetGlobalTime(){
+		currentGlobalTime=0;
+		return;
+	}
+	
 	//Advances all of the processes doing I/O by some amount of time (typically while some other process is running on CPU)
 	static private void advanceAllProcessesDoingIO(double TimeToAdvanceBy,int processNotToAdvance){
 		int searchNextProcessTimeIndex=0;
@@ -229,10 +254,11 @@ static public void addCPUTimeSlotOnProcess(int processID,int startTime,int endTi
 			if(i!=processNotToAdvance&&MProcessSchedules[i][0][1]!=-1){
 				
 			searchNextProcessTimeIndex=0;
-			while (processTime[i]>=MProcessSchedules[i][searchNextProcessTimeIndex][1]){
+			while (processTime[i]>=MProcessSchedules[i][searchNextProcessTimeIndex][1] && MProcessSchedules[i][searchNextProcessTimeIndex+1][1]!=-1){
 				searchNextProcessTimeIndex=searchNextProcessTimeIndex+1;
 			}
-			processTime[i]=Math.min(MProcessSchedules[i][searchNextProcessTimeIndex][1], processTime[i]+TimeToAdvanceBy);
+			processTime[i]=Math.min(Math.max(MProcessSchedules[i][searchNextProcessTimeIndex][0],processTime[i]),
+					processTime[i]+TimeToAdvanceBy);
 			
 			}
 		}	
